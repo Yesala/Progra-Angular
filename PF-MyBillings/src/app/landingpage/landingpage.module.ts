763634f39ffb3410edb991679f1ed48f8d9d1628
Yesala <ysalazarj@ucenfotec.ac.cr>
2022-08-11 15:24:35 -0600

    PF-MyBillings-LandingPage

---
+++ PF-MyBillings/src/app/landingpage/landingpage.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { LandingPageComponent } from './landingpage.component';
+
+@NgModule({
+    declarations: [
+        LandingPageComponent],
+    exports: [
+        LandingPageComponent
+    ],
+})
+export class LandingPageModule { }
